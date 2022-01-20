8d5ca22e3139971f9c019139be46b7d6de8695fc
Trevor Campbell <trevor.d.campbell@gmail.com>
2022-01-19 23:24:47 -0800

    fixed bugs in gaussian model

--- examples/common/model_gaussian.py
+++ examples/common/model_gaussian.py
@@ -1,9 +1,6 @@
 import numpy as np
 import scipy.linalg as sl
 
-def gen_synthetic(n, d, sig):
-  return 1. + sig*np.random.randn(n, d)
-
 def log_likelihood(x, th, sig):
   x = np.atleast_2d(x)
   th = np.atleast_2d(th)
@@ -17,19 +14,19 @@ def log_prior(th, mu0, sig0):
   return -th.shape[1]/2.*np.log(2*np.pi*sig0**2) - 1./2.*((th - mu0)**2).sum(axis=1)/sig0**2
 
 def log_joint(x, th, wts, sig, mu0, sig0):
-    return (wts[:, np.newaxis]*log_likelihood(x, th, sig)).sum(axis=0) + log_prior(th, mu0, sig0)
+  return (wts[:, np.newaxis]*log_likelihood(x, th, sig)).sum(axis=0) + log_prior(th, mu0, sig0)
 
 def grad_log_likelihood(x, th, sig):
   x = np.atleast_2d(x)
   th = np.atleast_2d(th)
-  return -(th[:, np.newaxis, :] - x[np.newaxis, :, :])/sig**2
+  return -(th[np.newaxis, : :] - x[:, np.newaxis, :])/sig**2
 
 def grad_log_prior(th, mu0, sig0):
   th = np.atleast_2d(th)
   return -(th - mu0)/sig0**2
 
 def grad_log_joint(x, th, wts, sig, mu0, sig0):
-  return (wts[:, np.newaxis]*grad_log_likelihood(x, th, sig)).sum(axis=1) + grad_log_prior(th, mu0, sig0)
+  return (wts[:, np.newaxis, np.newaxis]*grad_log_likelihood(x, th, sig)).sum(axis=0) + grad_log_prior(th, mu0, sig0)
 
 def hess_log_likelihood(x, th, sig):
   th = np.atleast_2d(th)
@@ -40,7 +37,7 @@ def hess_log_prior(th, mu0, sig0):
   return -np.ones((th.shape[0], th.shape[1]))/sig0**2
 
 def hess_log_joint(x, th, wts, sig, mu0, sig0):
-  return (wts[:, np.newaxis]*hess_log_likelihood(x, th, sig)).sum(axis=0) + hess_log_prior(th, mu0, sig0)
+  return (wts[:, np.newaxis, np.newaxis]*hess_log_likelihood(x, th, sig)).sum(axis=0) + hess_log_prior(th, mu0, sig0)
 
 def weighted_post(mu0, sig0, sig, x, w):
   sigp = np.sqrt(1./(1./sig0**2 + w.sum()/sig**2))
