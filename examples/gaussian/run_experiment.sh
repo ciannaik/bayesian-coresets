020622d04c285eeafc975f9b4612965a7bfad9b9
Trevor Campbell <trevor.d.campbell@gmail.com>
2022-01-20 16:14:04 -0800

    put full/lap runs in eralier loop in gaussian

--- examples/gaussian/run_experiment.sh
+++ examples/gaussian/run_experiment.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-## Full inference run for each trial (to establish a noise floor for evaluation metrics)
-#for ID in {1..10}
-#do
-#    python3 main.py --samples_inference 1000 --alg FULL --trial $ID run
-#done
+# Full inference run for each trial (to establish a noise floor for evaluation metrics)
+# Also do laplace 
+# Both of these cache results and re-use across coreset sizes later on during plotting
+for alg in "FULL" "LAP"
+do
+    for ID in {1..10}
+    do
+        python3 main.py --samples_inference 1000 --alg FULL --trial $ID run
+    done
+done
 
+# Run all the other algorithms
 for alg in "GIGA" "IHT"
 do
     for ID in {1..10}
